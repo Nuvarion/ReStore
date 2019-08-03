ce28858e79043e315525efd3c0d8376367158afc
Aleksandr Snigirev <aleksandrsnigirev@Aleksandrs-MacBook-Pro.local>
2019-08-03 22:05:06 +0500

    Refactoring + compose

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
+import { connect } from 'react-redux';
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
+import { compose } from '../../utils'; 
 
 import './book-list.css';
 
-export default class BookList extends Component {
+class BookList extends Component {
+
+    componentDidMount() {
+        const { bookstoreService } = this.props;
+        const data = bookstoreService.getBooks();
+
+        this.props.booksLoaded(data);
+    }
+
     render() {
         const { books } = this.props;
         return (
@@ -19,3 +31,17 @@ export default class BookList extends Component {
         );
     }
 }
+
+const mapStateToProps = ({ books }) => {
+    return { books };
+};
+
+const mapDispatchToProps = {
+    booksLoaded
+};
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+    )(BookList);
+    
--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -2,19 +2,9 @@ import React from 'react';
 import BookList from '../book-list';
 
 const HomePage = () => {
-    const books = [
-        { 
-            id: 1, 
-            title: 'Production-Ready Microservices',
-            author: 'Susan J. Fowler' },
-        {
-            id: 2,
-            title: 'Release It!',
-            author: 'Michael T. Nygard'
-        }
-    ]
     return (
-    <BookList books={books} />)
+    <BookList />
+    );
 };
 
 export default HomePage;
