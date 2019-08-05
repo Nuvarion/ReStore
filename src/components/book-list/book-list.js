c98fbbde82b8eda84c7329469e5a1c5cbfd0aad8
Aleksandr Snigirev <aleksandrsnigirev@Aleksandrs-MacBook-Pro.local>
2019-08-06 02:44:52 +0500

    Refactoring

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 import { withBookstoreService } from '../hoc';
-import { booksLoaded, booksRequested, booksError } from '../../actions';
+import { fetchBooks } from '../../actions';
 import { compose } from '../../utils'; 
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
@@ -12,18 +12,7 @@ import './book-list.css';
 class BookList extends Component {
 
     componentDidMount() {
-
-        const { 
-            bookstoreService, 
-            booksLoaded, 
-            booksRequested,
-            booksError } = this.props;
-
-        booksRequested();
-        bookstoreService.getBooks()
-            .then((data) =>  booksLoaded(data))
-            .catch((err) => booksError(err));
-  
+        this.props.fetchBooks();
     }
 
     render() {
@@ -49,16 +38,16 @@ class BookList extends Component {
             </ul>
         );
     }
-}
+};
 
 const mapStateToProps = ({ books, loading, error }) => {
     return { books, loading, error };
 };
 
-const mapDispatchToProps = {
-    booksLoaded,
-    booksRequested,
-    booksError
+const mapDispatchToProps = (dispatch, { bookstoreService }) => {
+    return {
+        fetchBooks: fetchBooks(bookstoreService, dispatch)
+    };
 };
 
 export default compose(
